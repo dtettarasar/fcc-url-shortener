2290228725ff8c4dd8a1893b32eb8bd1a28492f4
Replit user <>
2023-07-17 17:14:44 +0000

    fix db connexion

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const { MongoClient } = require('mongodb');
 
-const client = new MongoClient(process.env.(DB_URL));
+const client = new MongoClient(process.env.DB_URL);
 const db = client.db('fcc-url-shortener');
 const urls = db.collection('urlObj');
 
@@ -12,6 +12,10 @@ const urls = db.collection('urlObj');
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+//Middlewares to get json response from request
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -24,9 +28,12 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
 app.post('/api/shorturl', (req, res) => {
-  
+  console.log(req.body);
+  res.json({ greeting: 'hello API' });
 });
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
